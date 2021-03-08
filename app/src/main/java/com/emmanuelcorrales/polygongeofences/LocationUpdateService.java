369c09cfd8d5edb05edfd1cc85e0a2a7a46ad55c
knight1O1 <shashank.singh@live.in>
2021-03-09 04:26:46 +0530

    Gradle Config update & Migration to AndroidX.

--- app/src/main/java/com/emmanuelcorrales/polygongeofences/LocationUpdateService.java
+++ app/src/main/java/com/emmanuelcorrales/polygongeofences/LocationUpdateService.java
@@ -9,10 +9,10 @@ import android.location.Location;
 import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.NotificationCompat;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
--- app/src/main/java/com/emmanuelcorrales/polygongeofences/MainActivity.java
+++ app/src/main/java/com/emmanuelcorrales/polygongeofences/MainActivity.java
@@ -7,11 +7,11 @@ import android.graphics.Color;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
@@ -171,7 +171,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void update() {
-        if (mMarkers.size() <= 2) {
+        if (mMarkers.size() <= MIN_MARKER_SIZE) {
             return;
         }
         redraw();
@@ -179,7 +179,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void redraw() {
-        if (mMarkers.size() <= 2) {
+        if (mMarkers.size() <= MIN_MARKER_SIZE) {
             return;
         }
         if (mPolygon != null) {
@@ -196,7 +196,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (mCenterMarker != null) {
             mCenterMarker.remove();
         }
-        mCenterMarker = mMap.addMarker(new MarkerOptions().position(mCircle.getCenter()));
+        //mCenterMarker = mMap.addMarker(new MarkerOptions().position(mCircle.getCenter()));
     }
 
     private CircleOptions getCircularFence() {
